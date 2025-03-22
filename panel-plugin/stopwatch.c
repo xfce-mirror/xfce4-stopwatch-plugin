0f8bc1a06d6bed09770589878c88f85ff91f1d63
Bobby Rong <rjl931189261@126.com>
2025-03-22 22:59:04 +0800

    Add meson build

--- panel-plugin/stopwatch.c
+++ panel-plugin/stopwatch.c
@@ -5,6 +5,9 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+#ifdef HAVE_XFCE_REVISION_H
+#include "xfce-revision.h"
+#endif
 
 #include <libxfce4panel/libxfce4panel.h>
 #include <libxfce4util/libxfce4util.h>
@@ -236,7 +239,7 @@ stopwatch_show_about (XfcePanelPlugin *plugin, StopwatchPlugin *stopwatch)
 	gtk_show_about_dialog (NULL,
 		"logo-icon-name", "xfce4-stopwatch-plugin",
 		"license", xfce_get_license_text (XFCE_LICENSE_TEXT_BSD),
-		"version", PACKAGE_VERSION,
+		"version", VERSION_FULL,
 		"program-name", PACKAGE_NAME,
 		"comments", _("Time yourself"),
 		"website", PACKAGE_URL,
